e0d0cf6cc5444e68e6bc4ef62661bc97d3034796
devcraft1 <ukonudennis64@gmail.com>
2023-08-12 23:59:17 -1100

    --

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [UserModule, AdminModule],
+  imports: [UserModule],
   controllers: [],
   providers: [],
 })
